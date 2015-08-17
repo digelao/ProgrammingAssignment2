c61b1674401ca3acef0d07f385be3c4936bcc5bb
digelao <shixinggl@gmail.com>
2015-08-17 20:58:46 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,29 @@
 
--## Put comments here that give an overall description of what your
--## functions do
--
--## Caching the Inverse of a Matrix
--
--makeCacheMatrix <- function(x = matrix()) {
--    m <- NULL
--    get <- function() x
--    m <<- solve(x)
--   getinverse <- function() m 
--  list(get = get, getinverse = getinverse)
--}
--
--
--## cacheSolve first searches cache for the inverse, and then report or calculate the inverse
--## depending on availability of the inverve in the cache.
--
--cacheSolve <- function(x, ...) {
--       ## Return a matrix that is the inverse of 'x'
--  m <- x$getinverse()
--  if(!is.null(m)) {
--    message("getting cached data")
--    return(m)
--  }
--  data <- x$get()
--  m <- solve(data)
--  m
--  }
+## Put comments here that give an overall description of what your
+## functions do
+
+## Caching the Inverse of a Matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    get <- function() x
+    m <<- solve(x)
+   getinverse <- function() m 
+  list(get = get, getinverse = getinverse)
+}
+
+
+## cacheSolve first searches cache for the inverse, and then report or calculate the inverse
+## depending on availability of the inverve in the cache.
+
+cacheSolve <- function(x, ...) {
+       ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  m
+  }
